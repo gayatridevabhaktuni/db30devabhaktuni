be755394b166a2b56a2e9e4bfb74af53283f75ce
gayatridevabhaktuni <devabhaktunigayatri@gmail.com>
2021-11-10 18:50:54 -0600

    added

--- app.js
+++ app.js
@@ -60,38 +60,4 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function(){ 
  console.log("Connection to DB succeeded")}); 
 
- // We can seed the collection if needed on server start 
-async function recreateDB(){ 
-  // Delete everything 
-  await Costume.deleteMany(); 
- 
-  let instance1 = new 
-Costume({costume_type:"ghost",  size:'large', 
-cost:25.4}); 
-  instance1.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("First object saved") 
-  }); 
-
- 
-
-  let instance2 = new 
-Costume({costume_type:"Vampire",  size:'small', 
-cost:25.4}); 
-  instance2.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("Second object saved") 
-  }); 
-
-  let instance3 = new 
-Costume({costume_type:"Monster",  size:'Medium', 
-cost:25.4}); 
-  instance1.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("Third object saved") 
-  }); 
-} 
- 
-let reseed = true; 
-if (reseed) { recreateDB();} 
  
--- models/costume.js
+++ models/costume.js
@@ -5,5 +5,36 @@ const costumeSchema = mongoose.Schema({
  cost: Number 
 }) 
  
-module.exports = mongoose.model("Costume", 
-costumeSchema) 
+module.exports = mongoose.model("Costume", costumeSchema) 
+// We can seed the collection if needed on server start 
+async function recreateDB(){ 
+    // Delete everything 
+    await Costume.deleteMany(); 
+   
+    let instance1 = new 
+  Costume({costume_type:"ghost",  size:'large', cost:25.4}); 
+    instance1.save( function(err,doc) { 
+        if(err) return console.error(err); 
+        console.log("First object saved") 
+    }); 
+  
+   
+  
+    let instance2 = new 
+  Costume({costume_type:"Vampire",  size:'small', cost:25.4}); 
+    instance2.save( function(err,doc) { 
+        if(err) return console.error(err); 
+        console.log("Second object saved") 
+    }); 
+  
+    let instance3 = new 
+  Costume({costume_type:"Monster",  size:'Medium', cost:25.4}); 
+    instance1.save( function(err,doc) { 
+        if(err) return console.error(err); 
+        console.log("Third object saved") 
+    }); 
+  } 
+   
+  let reseed = true; 
+  if (reseed) { recreateDB();} 
+   
